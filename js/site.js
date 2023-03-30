98429c57faf9c28018af71c38e87f37837d0167a
cestrada3 <cestrada3@hawk.iit.edu>
2023-03-30 15:50:34 -0500

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,4 +1,9 @@
-jQuery(function($) {
-	$('html').removeClass('nojs');
-	$('html').addClass('hasjs');
-});
+function init(){
+
+	function Next_phase() {
+	  document.getElementById('des').innerHTML = 'Create Username and Password';
+	}
+	var b = document.getElementById('button-slot');
+	b.addEventListener('click', Next_phase);
+	}
+	window.addEventListener('load', init);
