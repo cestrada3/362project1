dee63c1ee5ca5c891827c8f7aef5ddb9790fbcbe
cestrada3 <cestrada3@hawk.iit.edu>
2023-04-06 14:22:58 -0500

    update the site java

--- js/site.js
+++ js/site.js
@@ -1,9 +1,29 @@
-function init(){
 
-	function Next_phase() {
-	  document.getElementById('des').innerHTML = 'Create Username and Password';
-	}
-	var b = document.getElementById('button-slot');
-	b.addEventListener('click', Next_phase);
-	}
-	window.addEventListener('load', init);
+const form = document.querySelector('#signup-form');
+const fullNameInput = document.querySelector('#fullName');
+const usernameInput = document.querySelector('#username');
+const emailInput = document.querySelector('#email');
+const phoneNumberInput = document.querySelector('#phoneNumber');
+const passwordInput = document.querySelector('#password');
+const confirmPasswordInput = document.querySelector('#confirmPassword');
+
+
+const emailError = document.querySelector('#email-error');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  if (!validateEmail()) {
+    emailError.innerText = 'Invalid email address';
+  } else {
+    emailError.innerText = '';
+    alert('Form submitted successfully!');
+    form.reset();
+  }
+});
+
+
+function validateEmail() {
+  const email = emailInput.value.trim();
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
